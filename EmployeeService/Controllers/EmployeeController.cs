20015ce4f05bc8b9a34b2cd87a428ad776fe5bb9
Kund0249 <k.kundan1999@gmail.com>
2024-03-23 11:55:47 +0530

    Add project files.

---
+++ EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService.Models;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private readonly EmployeeRepository repository;
+
+        public EmployeeController()
+        {
+            repository = new EmployeeRepository();
+        }
+        //public IEnumerable<EmployeeDTO> Get()
+        //{
+        //    return repository.GetEmployees;
+        //}
+
+        public HttpResponseMessage Get()
+        {
+            var data = repository.GetEmployees;
+            return Request.CreateResponse(HttpStatusCode.OK, new { Data = data });
+            //return repository.GetEmployees;
+        }
+    }
+}
---
+++ EmployeeService/Models/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeService.Models
+{
+    public class EmployeeRepository
+    {
+        public List<EmployeeDTO> GetEmployees
+        {
+            get
+            {
+                return new List<EmployeeDTO>()
+                {
+                    new EmployeeDTO(){EmployeeId=1,EmployeeName="A"},
+                    new EmployeeDTO(){EmployeeId=2,EmployeeName="B"},
+                    new EmployeeDTO(){EmployeeId=3,EmployeeName="C"},
+                    new EmployeeDTO(){EmployeeId=4,EmployeeName="D"},
+                };
+            }
+        }
+    }
+}
